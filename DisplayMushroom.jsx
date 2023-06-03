9aff4d0eb05fcae7dcd18fcd87113e2884dc169c
Tamir Polyakov <69022802+TamirPol@users.noreply.github.com>
2023-06-03 18:17:57 -0400

    Add files via upload

---
+++ DisplayMushroom.jsx
@@ -0,0 +1,24 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import styles from "../styles/styles";
+const DisplayMushroom = ({ mushroomType, asset }) => {
+  return (
+    <View style={[styles.container, styles.primary]}>
+      <View style={[styles.MushroomImageParent]}>
+        <Image
+          source={{ uri: asset.uri }}
+          style={[styles.displayMushroomImage]}
+        />
+      </View>
+
+      <View style={styles.mushroomTitle}>
+        <Text style={styles.titles}>{mushroomType.type}</Text>
+        <Text style={styles.titles}>
+          Percent likelihood: {mushroomType.prob}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default DisplayMushroom;
